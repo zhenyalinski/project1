a29ca062e45aea513263b05d7a5eb0fc5e491862
zheka <zvenglinski@gmail.com>
2022-03-08 01:37:20 -0500

    added text

--- tests/simple_pages_test.py
+++ tests/simple_pages_test.py
@@ -4,10 +4,10 @@ def test_request_main_menu_links(client):
     """This makes the index page"""
     response = client.get("/")
     assert response.status_code == 200
-    assert b'<a class="nav-link" href="/page1">Page 1</a>' in response.data
-    assert b'<a class="nav-link" href="/page2">Page 2</a>' in response.data
-    assert b'<a class="nav-link" href="/page3">Page 3</a>' in response.data
-    assert b'<a class="nav-link" href="/page4">Page 4</a>' in response.data
+    assert b'<a class="nav-link" href="/page1">Docker</a>' in response.data
+    assert b'<a class="nav-link" href="/page2">Heroku</a>' in response.data
+    assert b'<a class="nav-link" href="/page3">PyCharm</a>' in response.data
+    assert b'<a class="nav-link" href="/page4">GitHUB</a>' in response.data
 
 def test_request_index(client):
     """This makes the index page"""
